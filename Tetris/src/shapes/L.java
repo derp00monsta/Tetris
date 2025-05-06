409f38b2612df90db56a2264be1f51f3b16dbae3
Tina Hague <148731136+derp00monsta@users.noreply.github.com>
2025-05-06 15:23:00 -0700

    make java project

---
+++ Tetris/src/shapes/L.java
@@ -0,0 +1,39 @@
+package shapes;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+/**
+ * Filename: L.java
+ * 
+ * @author Tina Hague
+ * @version 1.0
+ * @since 1.0
+ * 
+ * Last Updated: 5 May 2025
+ */
+public class L extends Shape {
+
+    private static ArrayList<Point> coordinates = new ArrayList<Point>();
+    static {
+        coordinates.add(new Point(7, 0));
+        coordinates.add(new Point(7, 1));
+        coordinates.add(new Point(7, 2));
+        coordinates.add(new Point(8, 2));
+    }
+
+    /**
+     * Creates a new L shape.
+     */
+    public L() {
+        super(coordinates);
+    }
+    
+    @Override
+    /**
+     * String representation of the shape.
+     */
+    public String toString() {
+        return "L";
+    }
+}
---
+++ Tetris/src/shapes/Shape.java
@@ -0,0 +1,90 @@
+package shapes;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+/**
+ * Filename: Shape.java
+ * 
+ * @author Tina Hague
+ * @version 1.0
+ * @since 1.0
+ * 
+ * Last updated: 5 May 2025
+ */
+public class Shape {
+    private ArrayList<Point> coordinates;
+
+    public Shape(ArrayList<Point> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    /**
+     * Returns the coordinate positions of the shape.
+     * 
+     * @return array of coordinates of the shape.
+     */
+    public ArrayList<Point> getCoordinates() {
+        return coordinates;
+    }
+
+    /**
+     * Moves the shape left by one block.
+     */
+    public void moveLeft() {
+        for (int i = 0; i < coordinates.size(); i++) {
+            int x = (int) coordinates.get(i).getX() - 1;
+            int y = (int) coordinates.get(i).getY();
+            coordinates.get(i).setLocation(x, y);
+        }
+    }
+
+    /**
+     * Moves the shape right by one block.
+     */
+    public void moveRight() {
+        for (int i = 0; i < coordinates.size(); i++) {
+            int x = (int) coordinates.get(i).getX() + 1;
+            int y = (int) coordinates.get(i).getY();
+            coordinates.get(i).setLocation(x, y);
+        }
+    }
+
+    /**
+     * Moves the shape down by one block.
+     */
+    public void moveDown() {
+        for (int i = 0; i < coordinates.size(); i++) {
+            int x = (int) coordinates.get(i).getX();
+            int y = (int) coordinates.get(i).getY() + 1;
+            coordinates.get(i).setLocation(x, y);
+        }
+    }
+
+    /**
+     * Finds the x-coordinate of the leftmost edge of the shape.
+     * 
+     * @return the x-coordinate
+     */
+    public int getLeftEdge() {
+        return (int) coordinates.get(0).getX();
+    }
+
+    /**
+     * Finds the x-coordinate of the rightmost edge of the shape.
+     * 
+     * @return the x-coordinate
+     */
+    public int getRightEdge() {
+        return (int) coordinates.get(2).getX();
+    }
+
+    /**
+     * Finds the y-coordinate of the bottom edge of the shape.
+     * 
+     * @return the y-coordinate
+     */
+    public int getBottomEdge() {
+        return (int) coordinates.get(3).getY();
+    }
+}
